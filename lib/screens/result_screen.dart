59500d78f84001d68da3cf37a7da7c445a442ffc
Arkadiusz4 <arekmika4@gmail.com>
2022-07-29 23:59:36 +0200

    commit

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/widgets/dots.dart';
 
 class ResultScreen extends StatefulWidget {
-  const ResultScreen({Key? key}) : super(key: key);
+  ResultScreen({Key? key}) : super(key: key);
 
   @override
   State<ResultScreen> createState() => _ResultScreenState();
@@ -26,17 +27,62 @@ class _ResultScreenState extends State<ResultScreen> {
                   ),
                   height: MediaQuery.of(context).size.height * 0.55,
                 ),
-              ],
-            ),
-            Positioned(
-              bottom: 100,
-              child: Card(
-                child: Container(
-                  height: 100,
-                  width: MediaQuery.of(context).size.width - 80,
-                  color: Colors.red,
+                Positioned(
+                  bottom: 0,
+                  child: Card(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20)),
+                      height: 100,
+                      width: MediaQuery.of(context).size.width - 80,
+                      child: Column(children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Row(
+                              children: [
+                                Dots(color: Colors.blue),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Column(
+                                  children: const [
+                                    Text(
+                                      '100%',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w600,
+                                          color: Colors.blue),
+                                    ),
+                                    Text(
+                                      'Complete',
+                                      style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Text('Total Questions'),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text('Correct'),
+                            Text('Wrong'),
+                          ],
+                        )
+                      ]),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
           ],
         ),
